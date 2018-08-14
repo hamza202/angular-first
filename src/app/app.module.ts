95246299b8ea6ce2ebb9b79d30df0dc41a54d057
hamza202 <k8668975@gmail.com>
2018-08-14 15:54:55 +0300

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HamzaComponent } from './hamza/hamza.component';
 import { EventComponent } from './event/event.component';
 import { MyFormComponent } from './my-form/my-form.component';
-
+import { MyServiceComponentComponent } from './my-service-component/my-service-component.component';
+import { DataServiceService } from './service/data-service.service';
 @NgModule({
   declarations: [
     AppComponent,
     HamzaComponent,
     EventComponent,
-    MyFormComponent
+    MyFormComponent,
+    MyServiceComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $;
 @Component({
   selector: 'app-my-form',
   templateUrl: './my-form.component.html',
   styleUrls: ['./my-form.component.css']
 })
 export class MyFormComponent implements OnInit {
+  formName = '';
+  formLname = '';
+  formAge: number;
+    // myArray = ['hamzz' , 'welcome'];
 
+    // submitFunction() {
+    //   this.myArray.push(this.formName);
+    //   this.formName = '';
+    // }
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+$(function () {
+    $('[data-toggle="popover"]').popover();
+});
